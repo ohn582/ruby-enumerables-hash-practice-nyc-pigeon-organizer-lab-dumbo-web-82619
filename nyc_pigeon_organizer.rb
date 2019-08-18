4a9b109b11656598391969ed063386b4523e13b7
Neal JIn Oh <no4259@gmail.com>
2019-08-18 16:27:36 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,25 +1,4 @@
-  pigeon_data = {
-        :color => {
-          :purple => ["Theo", "Peter Jr.", "Lucky"],
-          :grey => ["Theo", "Peter Jr.", "Ms. K"],
-          :white => ["Queenie", "Andrew", "Ms. K", "Alex"],
-          :brown => ["Queenie", "Alex"]
-        },
-        :gender => {
-          :male => ["Alex", "Theo", "Peter Jr.", "Andrew", "Lucky"],
-          :female => ["Queenie", "Ms. K"]
-        },
-        :lives => {
-          "Subway" => ["Theo", "Queenie"],
-          "Central Park" => ["Alex", "Ms. K", "Lucky"],
-          "Library" => ["Peter Jr."],
-          "City Hall" => ["Andrew"]
-        }
-  }
-
-
 def nyc_pigeon_organizer(data)
-  
   
   names = []
   pigeon_hash = {}
@@ -45,6 +24,5 @@ def nyc_pigeon_organizer(data)
   end
   
   pigeon_hash
-  
   
 end
